c888deac67587bd40e5735515303f47c22cb489d
Otávio Cipriano <otavio.b.cipriano@outlook.com>
2021-12-24 06:38:54 -0300

    add yml file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,12 @@ import Navbar from '../components/Navbar/Navbar'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar/>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Navbar />
       <Component {...pageProps} />
     </>
   )
