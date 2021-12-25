8d37840afdc9034849c5c5c471dde6e94791204e
Otávio Cipriano <otavio.b.cipriano@outlook.com>
2021-12-25 17:06:09 -0300

    migrate to yarn from npm

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -5,11 +5,11 @@ import matter from 'gray-matter'
 import {marked} from 'marked'
 import PostBody from '../../components/PostBody/PostBody';
 
-export default function PostPage({frontmatter: {title, date, cover_image}, slug, post}) {
+export default function PostPage({frontmatter: {title, date, cover_image}, slug, content}) {
     return (
         <div>
             <PostBody title={title} cover_image={cover_image} date={date}>
-                <div dangerouslySetInnerHTML={{__html: post}}></div>
+                <div dangerouslySetInnerHTML={{__html: marked(content)}}></div>
             </PostBody>
         </div>
     )
@@ -36,13 +36,12 @@ export async function getStaticProps({params: {slug}}){
     const markdownMeta = fs.readFileSync(path.join('src/posts', `${slug}.md`), 'utf-8')
     
     const {data: frontmatter, content} = matter(markdownMeta)
-    const post = marked(content)
     
     return{
         props: {
             frontmatter,
             slug,
-            post
+            content
         }
     }
 }
