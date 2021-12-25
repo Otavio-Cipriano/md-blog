c5a07639a23e8e894ff2d42a83b812e2a203edb3
Otávio Cipriano <otavio.b.cipriano@outlook.com>
2021-12-25 10:00:08 -0300

    testing build

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,19 +1,23 @@
 import '../styles/globals.css'
 
-import Head from 'next/head'
+import { Html, Head } from 'next/document'
 
 import Navbar from '../components/Navbar/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
-      </Head>
-      <Navbar />
-      <Component {...pageProps} />
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Navbar />
+          <Component {...pageProps} />
+        </body>
+      </Html>
     </>
   )
 }
