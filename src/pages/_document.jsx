fe211d4ce0f21b6d47b140a5b26fda08b66053c4
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2021-12-26 14:29:46 -0300

    update seo of blog

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,8 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <meta name="description" content="markdown blog createad as a study case for a free lance"/>
+                <meta name="keywords" content="HTML, CSS, JavaScript, NextJs, Markdown, Markdown blog, blog, simple blog, study case" />
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
